d6436a30460cb419eb43ceca79a36c7fd57e4e41
shyammodi11 <72390323+shyammodi11@users.noreply.github.com>
2022-04-21 22:19:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+
+app = Flask(__name__)
+model = pickle.load(open('r1.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    if output < 0:
+        output = 0
+
+    return render_template('index.html', prediction_text='Estimated Insurance Cost: ${}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
